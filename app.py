da000992fe6e2db40e78f0e63c16842790cb8f2a
dengzz <707643733@qq.com>
2023-12-07 23:16:26 +0800

    .

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/', static_folder='static',template_folder='templates')
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index-0.html')
 
 
 if __name__ == '__main__':
